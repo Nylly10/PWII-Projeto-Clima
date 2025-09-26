14f46e822f10d04237037ddfa75de42f82ed5f0f
Nylly Anny <nylly.yaya@gmail.com>
2025-09-26 15:32:47 -0300

    Delete Projeto3 directory

--- Projeto3/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.busca').addEventListener('submit', (event)=>{
-    event.preventDefault()
-
-    let input = document.querySelector('#searchInput').value
-
-    console.log(input)
-})
